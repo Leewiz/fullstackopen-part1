95b66b1d0f775470a498bf5f1a2cfff9504269a1
leewiz <greeneggsnham59@gmail.com>
2021-09-07 12:34:37 -0500

    complete part 1.4 parts as array

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.text}</h1>
+      <h1>{props.course}</h1>
     </div>
   )
 }
@@ -28,31 +28,32 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Number of exercises {props.count}
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
-
+  ]
   return (
     <div>
-      <Header text={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
